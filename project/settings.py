699573c81277f2ecebc4cd760dd60203fe9b9ef7
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-01-27 20:03:31 +0300

    add

--- project/settings.py
+++ project/settings.py
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': "django.db.backends.postgresql",
+        'HOST': "aws-0-eu-central-1.pooler.supabase.com",
+        'NAME': "postgres",
+        'USER': "postgres.hnfhoxgvhdxicrmypedy",
+        'PASSWORD': "7427999qQ$%",
+        'PORT': "6543",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
