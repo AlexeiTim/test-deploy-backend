7774d7e247a6e4f0d8fe5dec23c66dfa691a9356
AlexeiTim <86958614+AlexeiTim@users.noreply.github.com>
2024-01-27 19:42:24 +0300

    add

--- core/views.py
+++ core/views.py
@@ -3,6 +3,10 @@ from rest_framework import viewsets
 from core import serializers, models
 
 
+def index(request):
+    return render(request, 'core/index.html')
+
+
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = models.Todo.objects.all()
     serializer_class = serializers.TodoSerializer
