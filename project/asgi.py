f4af62f0b7582f57409f00aad1c8d6dd804c86b8
Алексей Тимашков <timashkov060896@gmail.com>
2024-02-06 19:45:05 +0300

    сменил расположение файлов

--- project/asgi.py
+++ project/asgi.py
@@ -2,6 +2,7 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+from channels.security.websocket import AllowedHostsOriginValidator
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
@@ -9,12 +10,15 @@ django_asgi_app = get_asgi_application()
 
 import core.routing
 
+django.setup()
+
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
-    "websocket":
+    "websocket": AllowedHostsOriginValidator(
         URLRouter(
             core.routing.websocket_urlpatterns
         ),
+    )
 })
 
 app = application
