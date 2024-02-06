7a7eafe6560f1014aa17fdf0d231714a35c83a15
Алексей Тимашков <timashkov060896@gmail.com>
2024-02-06 15:35:30 +0300

    пофиксил настройкии

--- core/routing.py
+++ core/routing.py
@@ -6,7 +6,7 @@ from django.urls import path
 
 from . import consumers
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
