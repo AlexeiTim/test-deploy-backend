29aeaae25049ee484d6b10bdab71eacb572e1d17
Алексей Тимашков <timashkov060896@gmail.com>
2024-02-06 20:11:46 +0300

    добавил консумер

--- project/asgi.py
+++ project/asgi.py
@@ -2,15 +2,13 @@ import os
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
-from channels.security.websocket import AllowedHostsOriginValidator
 import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 django_asgi_app = get_asgi_application()
 
+from channels.security.websocket import AllowedHostsOriginValidator
 import core.routing
-
-django.setup()
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
