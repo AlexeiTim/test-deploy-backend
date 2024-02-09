1aded6728170f94d134f9e3634a3e696a239b213
Алексей Тимашков <timashkov060896@gmail.com>
2024-02-09 10:32:47 +0300

    добавил консумер

--- project/settings.py
+++ project/settings.py
@@ -100,6 +100,9 @@ DATABASES = {
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        "CONFIG": {
+            "hosts": [("localhost", 6379)],
+        },
     },
 }
 
